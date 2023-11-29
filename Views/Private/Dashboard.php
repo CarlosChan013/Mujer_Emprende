bfb35cdac24a2d164f3ef3007430fb9e107c753b
CarlosChan013 <106627780+CarlosChan013@users.noreply.github.com>
2023-11-29 16:42:17 -0500

    Ignoren esto

--- Views/Private/Dashboard.php
+++ Views/Private/Dashboard.php
@@ -1,13 +1,11 @@
 <?php
-// Inicia la sesión para acceder a la información del usuario
-session_start();
-
-// Verifica si el usuario está autenticado
-if (!isset($_SESSION['id'])) {
-    // Si no está autenticado, redirige a la página de inicio de sesión u otra página
-    header("Location: ../Views/Login.php");
-    exit();
-}
+// Simulación de usuarios (reemplaza con tu lógica de obtención de usuarios)
+$usuarios = array(
+    array('id' => 1, 'nombre' => 'Usuario 1'),
+    array('id' => 2, 'nombre' => 'Usuario 2'),
+    array('id' => 3, 'nombre' => 'Usuario 3'),
+    // ... más usuarios ...
+);
 ?>
 
 <!DOCTYPE html>
@@ -16,11 +14,61 @@ if (!isset($_SESSION['id'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Agregar la referencia de Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Agregar la referencia de FontAwesome para los íconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="css/Styles.css">
     <title>Dashboard Administradores</title>
 </head>
-
 <body>
 
+<!-- Barra de navegación usando Bootstrap -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Dashboard</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cerrar Sesión</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!-- Contenido principal -->
+<div class="container mt-4">
+    <h2>Lista de Usuarios</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre de usuario</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($usuarios as $usuario): ?>
+                <tr>
+                    <td><?php echo $usuario['id']; ?></td>
+                    <td><?php echo $usuario['nombre']; ?></td>
+                    <td>
+                        <!-- Botones de acción con íconos de FontAwesome -->
+                        <a href="#" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i> Editar</a>
+                        <a href="#" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i> Eliminar</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
+<!-- Agregar la referencia de Bootstrap JS y Popper.js  -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
-</html> 
+</html>
