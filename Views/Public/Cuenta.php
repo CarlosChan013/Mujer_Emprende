0c08ca1f79682cc282bd4e1aa14a8fc6dd79ac41
KevinRM2 <21393118@utcancun.edu.mx>
2023-11-29 16:57:44 -0500

    prueba
    a ver

--- Views/Public/Cuenta.php
+++ Views/Public/Cuenta.php
@@ -1,56 +1,191 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cuenta</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f2f2f2;
+        }
+
+        /* Estilos para el header */
+        header {
+            
+            color: white;
+            padding: 10px;
+            text-align: center;
+        }
+
+        /* Estilos para las secciones */
+        section {
+            padding: 20px;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        section div {
+            background-color: white;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1, h2, h3 {
+            color: #333;
+        }
+
+        p {
+            color: #666;
+        }
+
+        .cta {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #E68989;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+            margin-top: 15px;
+            transition: background-color 0.3s ease;
+        }
+
+        .cta:hover {
+            background-color: #d66;
+        }
+
+        a {
+            color: #E68989;
+            text-decoration: none;
+            font-weight: bold;
+        }
+
+        a:hover {
+            color: #d66;
+        }
+        footer {
+          background-color: #999BCC;
+            color: white;
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        .footer-content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .footer-section {
+            flex: 1;
+            margin: 10px;
+        }
+
+        .footer-divider {
+            border-left: 1px solid #fff;
+            height: 40px;
+            margin: 0 20px;
+        }
+
+        .footer-section h2 {
+            font-size: 1.2em;
+        }
+
+        .footer-bottom {
+            margin-top: 20px;
+        }
+
+        .conditions-link {
+            color: #fff;
+            text-decoration: none;
+        }
+    </style>
 </head>
+
 <body>
-<!---------------------------- Header ----------------------------------->
-<?php include('../../Include/Header.html'); ?>
-    <section >
-         <div >
-              <div >
-                   <div >
-                   <h2>Bienvenid@s</h2>
-                   <p>¿Tienes una cuenta? inicia sesion </p>
-                   <h3><a href="../Public/Login.php" class="cta">Iniciar sesión</a></h3>
-                   </div>
-               </div>
-         </div>
+    <!-- Header -->
+    <header>
+        <?php include('../../Include/Header.html'); ?>
+    </header>
+
+    <!-- Secciones de registro -->
+    <section>
+        <div>
+            <div>
+                <div>
+                    <h2>Bienvenid@s</h2>
+                    <p>¿Tienes una cuenta? Inicia sesión</p>
+                    <h3><a href="../Public/Login.php" class="cta">Iniciar sesión</a></h3>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <section >
-          <div>
-              <div>
-                   <div>
+    <section>
+        <div>
+            <div>
+                <div>
                     <h1>¿No tienes una cuenta?</h1>
-                   <p class="cuenta-gratis">Es 100% gratis</p>
-                   <h3><a href="../Public/Registro.php">Registrarse</a></h3>
-                   </div>
-              </div>
-           </div>
+                    <p class="cuenta-gratis">Es 100% gratis</p>
+                    <h3><a href="../Public/Registro.php">Registrarse</a></h3>
+                </div>
+            </div>
+        </div>
     </section>
 
     <section>
-         <div>
-              <div>
-                  <div> 
+        <div>
+            <div>
+                <div>
                     <h1>¿Quieres ofrecer tus productos y publicar historias?</h1>
-                    <h3><a href="../Public/Registro_proveedora.php">Registrate aqui</a></h3>
-                  </div>
-              </div>
-         </div>
+                    <h3><a href="../Public/Registro_proveedora.php">Regístrate aquí</a></h3>
+                </div>
+            </div>
+        </div>
     </section>
-    
-    <section>
-        <h2> Registrate con google  </h2>
-        
-    </section>
- <footer>
 
- </footer>
- 
+    <section>
+        <h2>Regístrate con Google</h2>
+        <!-- Puedes agregar contenido para el registro con Google si es necesario -->
+    </section>
+
+    <!-- Footer -->
+   <!-- Footer -->
+   <footer>
+        <div class="footer-content">
+            <div class="footer-section">
+                <h2>Dirección</h2>
+                <p>Av Miguel Hidalgo ruta 5</p>
+            </div>
+            <div class="footer-divider"></div>
+            <div class="footer-section">
+                <h2>Número de teléfono</h2>
+                <p>+52 9985547381</p>
+            </div>
+            <div class="footer-divider"></div>
+            <div class="footer-section">
+                <h2>EMAIL</h2>
+                <p>tostaruydo@gufum.com</p>
+            </div>
+            <div class="footer-divider"></div>
+            <div class="footer-section">
+                <h2>Equipo de desarrollo:</h2>
+                <p>TSU. Chan Lugo Carlos Manuel</p>
+                <p>TSU. Perez Couoh Jose Angel</p>
+                <p>TSU. García Hernandez Ricardo</p>
+                <p>TSU. Ramirez Morales Kevin Rigoberto</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <a href="ruta-a-tu-pagina-de-condiciones.html" class="conditions-link">Copyrights@MujerEmprende2023</a>
+        </div>
+    </footer>
 </body>
-</html>  
+
+</html>
--- Views/Public/Mercado.php
+++ Views/Public/Mercado.php
@@ -1,91 +1,188 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/Styles.css">
     <title>Mercado</title>
+
+    <style>
+        /* Estilos para hacer la página más responsiva */
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        /* Estilos para el header */
+        header {
+            background-color: #f2f2f2;
+            padding: 10px;
+            text-align: center;
+        }
+
+        /* Estilos para las secciones */
+        section {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+            margin: 20px auto;
+            max-width: 1200px;
+        }
+
+        /* Estilos para las tarjetas de productos */
+        .card {
+            border: 1px solid #000;
+            border-radius: 10px;
+            margin: 10px;
+            padding: 10px;
+            width: calc(30% - 20px); /* Ajuste del ancho según las preferencias */
+            text-align: center;
+            background-color: #fff;
+        }
+
+        .product-image {
+            max-height: 100px;
+            margin-bottom: 10px;
+            width: 100%;
+            object-fit: cover;
+        }
+
+        .buy-button {
+            background-color: #E68989;
+            color: white;
+            border: none;
+            padding: 5px 10px;
+            border-radius: 5px;
+            cursor: pointer;
+            text-decoration: none;
+            display: inline-block;
+            margin-top: 10px;
+        }
+
+        /* Estilos para el footer */
+        footer {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        .footer-content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .footer-section {
+            flex: 1;
+            margin: 10px;
+        }
+
+        .footer-divider {
+            border-left: 1px solid #fff;
+            height: 40px;
+            margin: 0 20px;
+        }
+
+        .footer-section h2 {
+            font-size: 1.2em;
+        }
+
+        .footer-bottom {
+            margin-top: 20px;
+        }
+
+        .conditions-link {
+            color: #fff;
+            text-decoration: none;
+        }
+    </style>
 </head>
+
 <body>
 
-<?php
-include("../../Conect.php");
-session_start();
-
-// Verificar si el usuario está logeado
-if (isset($_SESSION['id'])) {
-    // Mostrar el header para usuarios logeados
-    include('../../include/Login_header.html');
-} else {
-    // Mostrar el header normal para usuarios no logeados
-    include('../../include/Header.html');
-}
-?>
-
-<section>
-    <img class="Fondo_mercado" src="../../Assets/Img/Fondo_mercado.png" alt="Fondo_mercado">
-</section>
-
-<!-- Aquí va un pequeño ordenador, por ejemplo, si aprieta a precio, lo puede seleccionar para que
-aparezca de más caro a barato y viceversa, para ordenar los productos que se ofrecen -->
-<style>
-    .card {
-        border: 1px solid #000;
-        border-radius: 10px;
-        margin: 10px;
-        padding: 10px;
-        width: 200px; /* Ajusta el ancho según tus preferencias */
-        display: inline-block;
-        text-align: center;
-        background-color: #fff;
-    }
-
-    .product-image {
-        max-height: 100px;
-        margin-bottom: 10px;
-    }
-
-    .buy-button {
-        background-color: #E68989;
-        color: white;
-        border: none;
-        padding: 5px 10px;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-</style>
-
-<section>
     <?php
-    $Consulta = "SELECT Codigo_producto, Titulo_producto, Contenido_producto, Precio_producto, Foto_producto FROM Productos";
-    $Resultado = $Conexion->query($Consulta);
+    include("../../Conect.php");
+    session_start();
 
-    while($row = $Resultado->fetch_assoc()) { ?>
-        <div class="card">
-            <img class="product-image" src="data:image/<?php echo pathinfo($row['Foto_producto'], PATHINFO_EXTENSION); ?>;base64, <?php echo base64_encode($row['Foto_producto']); ?>" alt="Producto">
-            <h3><?php echo $row['Titulo_producto']; ?></h3>
-            <p><?php echo $row['Contenido_producto']; ?></p>
-            <p>Precio: <?php echo $row['Precio_producto']; ?></p>
-            <a class="buy-button" href="./Producto_detalles.php?id=<?php echo $row['Codigo_producto']; ?>">Comprar</a>
-        </div>
-    <?php } ?>
-</section>
-
-<script>
-    function addToCart(productTitle) {
-        // Puedes agregar lógica adicional aquí para manejar la adición al carrito
-        // Por ahora, simplemente redireccionamos a la página del carrito
-        window.location.href = './Producto.php';
+    // Verificar si el usuario está logeado
+    if (isset($_SESSION['id'])) {
+        // Mostrar el header para usuarios logeados
+        include('../../include/Login_header.html');
+    } else {
+        // Mostrar el header normal para usuarios no logeados
+        include('../../include/Header.html');
     }
-</script>
+    ?>
 
-<!-- Agregar un botón para que lo redirija al formulario para publicar una historia (solo si está logeado) -->
-<div>
-    <button type="button"><a href="../../Views_publicar/Vista_producto.php">Publicar producto</a></button>
-</div>
+    <section>
+        <img class="Fondo_mercado" src="../../Assets/Img/Fondo_mercado.png" alt="Fondo_mercado">
+    </section>
 
-<!-- Footer -->
-<?php include('../../Include/Footer.html'); ?>
+    <section>
+        <?php
+        $Consulta = "SELECT Codigo_producto, Titulo_producto, Contenido_producto, Precio_producto, Foto_producto FROM Productos";
+        $Resultado = $Conexion->query($Consulta);
+
+        while ($row = $Resultado->fetch_assoc()) { ?>
+            <div class="card">
+                <img class="product-image" src="data:image/<?php echo pathinfo($row['Foto_producto'], PATHINFO_EXTENSION); ?>;base64, <?php echo base64_encode($row['Foto_producto']); ?>" alt="Producto">
+                <h3><?php echo $row['Titulo_producto']; ?></h3>
+                <p><?php echo $row['Contenido_producto']; ?></p>
+                <p>Precio: <?php echo $row['Precio_producto']; ?></p>
+                <a class="buy-button" href="./Producto_detalles.php?id=<?php echo $row['Codigo_producto']; ?>">Comprar</a>
+            </div>
+        <?php } ?>
+    </section>
+
+    <script>
+        function addToCart(productTitle) {
+            // Puedes agregar lógica adicional aquí para manejar la adición al carrito
+            // Por ahora, simplemente redireccionamos a la página del carrito
+            window.location.href = './Producto.php';
+        }
+    </script>
+
+    <!-- Agregar un botón para que lo redirija al formulario para publicar una historia (solo si está logeado) -->
+    <div style="text-align: center; margin-top: 20px;">
+        <button type="button"><a href="../../Views_publicar/Vista_producto.php">Publicar producto</a></button>
+    </div>
+
+    <!-- Footer -->
+    <footer>
+        <div class="footer-content">
+            <div class="footer-section">
+                <h2>Dirección</h2>
+                <p>Av Miguel Hidalgo ruta 5</p>
+            </div>
+            <div class="footer-divider"></div>
+            <div class="footer-section">
+                <h2>Número de teléfono</h2>
+                <p>+52 9985547381</p>
+            </div>
+            <div class="footer-divider"></div>
+            <div class="footer-section">
+                <h2>EMAIL</h2>
+                <p>tostaruydo@gufum.com</p>
+            </div>
+            <div class="footer-divider"></div>
+            <div class="footer-section">
+                <h2>Equipo de desarrollo:</h2>
+                <p>TSU. Chan Lugo Carlos Manuel</p>
+                <p>TSU. Perez Couoh Jose Angel</p>
+                <p>TSU. García Hernandez Ricardo</p>
+                <p>TSU. Ramirez Morales Kevin Rigoberto</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <a href="ruta-a-tu-pagina-de-condiciones.html" class="conditions-link">Copyrights@MujerEmprende2023</a>
+        </div>
+    </footer>
 </body>
+
 </html>
